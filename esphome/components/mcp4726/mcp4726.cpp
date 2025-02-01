414e43779be402089ba8c54a367c13496ad3e638
az <azza@fastmai.com>
2025-02-01 21:45:12 +1100

    comment out dupe define

--- esphome/components/mcp4726/mcp4726.cpp
+++ esphome/components/mcp4726/mcp4726.cpp
@@ -30,7 +30,7 @@ void mcp4726::dump_config() {
 void mcp4726::write_state(float state) {
  // const uint16_t value = (uint16_t) round(state * (pow(2, mcp4726_RES) - 1));
  // uint16_t output = (uint16_t) remap((1024-value), 0, 1024, 2100, 3350);
- const uint16_t value = (uint16_t) round(state * (pow(2, mcp4726_RES) - 1));
+ //const uint16_t value = (uint16_t) round(state * (pow(2, mcp4726_RES) - 1));
 
   int value = state * 1024;
     //Map to 2100-3350, values by trial and error, may depend on used light. I am using LEDs. 
